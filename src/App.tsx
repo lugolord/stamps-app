a61074c27dcec0ef7ceed72e1569003b646f0da4
Luis Gonzalez <lugo50635@gmail.com>
2026-08-31 20:13:23 -0300

    feat: setup react router and add register view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router";
 import Login from './views/Login'
+import SignUp from './views/SignUp';
 
 function App() {
   return (
     <>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/views/SignUp.tsx
+++ src/views/SignUp.tsx
@@ -1,20 +1,24 @@
 import { Link } from 'react-router'
 
-function Login () {
+function SignUp () {
   return (
     <div className='flex flex-col items-center justify-evenly h-screen'>
       <h1 className='text-4xl'>L'aura café</h1>
-      <h2 className='text-2xl'>Bienvenido</h2>
+      <p className='text-gray-500'>Crea tu cuenta y empieza a acumular sellos 😄</p>
       <form className='flex flex-col border w-9/10 p-5 rounded'>
+        <label htmlFor="name">Name</label>
+        <input className='border mb-3 p-2 rounded' id='name' type="text" placeholder='Pepito' />
+        <label htmlFor="lastname">Last name</label>
+        <input className='border mb-3 p-2 rounded' id='lastname' type="text" placeholder='Perez' />
         <label htmlFor="email">Email</label>
         <input className='border mb-3 p-2 rounded' id='email' type="email" placeholder='user@email.com' />
         <label htmlFor="password">Password</label>
         <input className='border mb-3 p-2 rounded' id='password' type="password" placeholder='password' />
-        <button className='bg-black text-white rounded py-2 cursor-pointer'>login</button>
+        <button className='bg-black text-white rounded py-2 cursor-pointer'>Crear cuenta</button>
       </form>
-      <p>No tienes cuenta? <Link to='/register'>Registrate</Link></p>
+      <Link to='/'>Volver al login</Link>
     </div>
   )
 }
 
-export default Login
+export default SignUp
